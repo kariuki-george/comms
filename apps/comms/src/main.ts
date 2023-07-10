2b8180ed9a57379c79a996d3f404259b92226350
smiley-geek <kariukigeorge2030@gmail.com>
2023-07-10 09:07:18 +0000

    fix: fixed cors errors

--- apps/comms/src/main.ts
+++ apps/comms/src/main.ts
@@ -10,9 +10,21 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const configService = app.get<ConfigService>(ConfigService);
   initAdapters(app);
-  app.enableCors({ origin: '*', allowedHeaders: ['aid'] });
-  await app.listen(configService.getOrThrow<'number'>('PORT'), () => {
-    console.log('Comms server started successfully');
+  app.enableCors({
+    origin:
+      configService.get<string>('NODE_ENV') &&
+      configService.get<string>('NODE_ENV') === 'production'
+        ? [
+            'https://comms.p.kariukigeorge.me',
+            'https://comms-test.p.kariukigeorge.me',
+          ]
+        : '*',
+  });
+  await app.listen(configService.get('PORT'), () => {
+    console.log(
+      'Comms server started successfully on port ',
+      configService.get('PORT'),
+    );
   });
 }
 bootstrap();
